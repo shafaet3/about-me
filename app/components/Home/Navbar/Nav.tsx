e8d2b68a9d495b01e1e0cbb4650e81dcea024f95
shafaet <shafaet3@gmail.com>
2025-10-31 11:09:48 +0600

    initial setup and nav menu done

---
+++ app/components/Home/Navbar/Nav.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { Navlinks } from '@/app/constant/constant'
+import Link from 'next/link'
+import React from 'react'
+import { BiDownload } from 'react-icons/bi'
+import { FaCode } from 'react-icons/fa'
+import { HiBars3BottomRight } from 'react-icons/hi2'
+
+const Nav = () => {
+  return (
+    <div className='transition-all duration-200 h-[12vh] z-[10000] fixed w-full'>
+      <div className="flex items-center justify-between h-full w-[90%] mx-auto">
+        {/* Logo */}
+        <div className="flex items-center space-x-2">
+          <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center flex-col">
+            <FaCode className='w-5 h-5 text-black' />
+          </div>
+          <h1 className='text-xl hidden sm:block md:text-2xl text-white font-bold'> SHAFAET </h1>
+        </div>
+        {/* Navigation Links */}
+        <div className="hidden lg:flex items-center space-x-10">
+          {Navlinks.map((link) => {
+            return (
+              <Link
+                key={link.id}
+                href={link.url}
+                className='text-base hover:text-cyan-300 text-white font-medium transition-all duration-200'
+              >
+                <p>{link.label}</p>
+              </Link>
+            );
+          })}
+        </div>
+        {/* Download CV Button */}
+        <div className="flex items-center space-x-4">
+          <button className="px-8 py-3.5 text-sm cursor-pointer rounded-lg bg-blue-800 text-white hover:bg-blue-900 transition-all duration-300 flex items-center space-x-2">
+            <BiDownload className='w-5 h-5' />
+            <span>Download CV</span>
+          </button>
+          <HiBars3BottomRight className='w-8 h-8 text-white lg:hidden cursor-pointer' />
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Nav
